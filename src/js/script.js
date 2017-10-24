4d82853e5b1096a97dca58f9bee26346d52969b0
Olga Diianova <olga.diianova@gmail.com>
2017-10-24 22:53:00 +0200

    styles for the entire page and humburger menu for small screens

---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+(function () {
+  var menuBtn = document.querySelector('.js-menu-bnt');
+  var menuBtnIcon = menuBtn.querySelector('.js-menu-btn-icon');
+  var menuList = document.querySelector('.js-menu');
+
+  menuBtn.addEventListener('click', function(){
+    menuList.classList.toggle('menu_show');
+    menuBtnIcon.classList.toggle('menu-bnt__icon_show');
+  });
+})();
