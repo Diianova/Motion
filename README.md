5c0bcc003e1397e69cbda80b32d521bfaca295de
Olga Diianova <olga.diianova@gmail.com>
2017-10-27 16:26:47 +0200

    created slider for main banner

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Motion
-Creating Motion landing page using Smartgrid layout, which is a new method of  preprocessor grid created by Dmitry Lavrik. Using Smartgrid we do not adding classes in the html code like in the bootstrap, but only add the mixins to the existing selectors. The accomplished layout in this project is being done by using HTML, BEM, SASS(SCSS), Smartgrid, Flexbox, Gulp.
+Creating Motion landing page using Smartgrid layout, which is a new method of  preprocessor grid created by Dmitry Lavrik. Using Smartgrid we do not adding classes in the html code like in the bootstrap, but only add the mixins to the existing selectors. The accomplished layout in this project is being done by using HTML(BEM), SASS(SCSS), JS, Smartgrid, Flexbox, Gulp.
--- src/js/script.js
+++ src/js/script.js
@@ -10,5 +10,77 @@
 	    menuBtnIcon.classList.toggle('menu-bnt__icon_show');
 	});
 
+	//Slaider for main banner
+	var sl = new Slider('.carousel__item');
+
+    var btn_prev = document.querySelector('.button_prev');
+    var btn_next = document.querySelector('.button_next');
+
+    btn_prev.onclick = function () {
+        sl.prev();
+    }
+
+    btn_next.onclick = function () {
+        sl.next();
+    }
+
 })();
 
+
+function Slider(img) {
+    this.images = document.querySelectorAll(img);
+    this.i = 1;
+
+    this.next = function () {
+        this.images[this.i].classList.remove('carousel__item_main-pos');
+        this.images[this.i].style.right="auto";
+        this.images[this.i].style.left="auto";
+        this.i--;
+
+        if (this.i < 0) {
+            this.i = this.images.length - 1;
+        }
+
+        this.images[this.i].classList.add('carousel__item_main-pos');
+        this.images[this.i].style.left="50%";
+
+        if(this.i == "0"){
+        	this.images[this.i+1].style.right="0";
+        	this.images[this.i+2].style.left="0";
+        }else if(this.i == "1"){
+        	this.images[this.i+1].style.right="0";
+        	this.images[this.i-1].style.left="0";
+        }else{
+        	this.images[this.i-2].style.right="0";
+        	this.images[this.i-1].style.left="0";
+        }
+    }
+
+    this.prev = function () {
+        this.images[this.i].classList.remove('carousel__item_main-pos');
+        this.i++;
+
+        if (this.i >= this.images.length) {
+            this.i = 0;
+        }
+
+        this.images[this.i].classList.add('carousel__item_main-pos');
+        this.images[this.i].style.right="auto";
+        this.images[this.i].style.left="50%";
+
+        if(this.i == "0"){
+        	this.images[this.i+1].style.right="0";
+        	this.images[this.i+1].style.left="auto";
+        	this.images[this.i+2].style.left="0";
+        }else if(this.i == "1"){
+        	this.images[this.i+1].style.right="0";
+        	this.images[this.i+1].style.left="auto";
+        	this.images[this.i-1].style.left="0";
+        	this.images[this.i-1].style.rigth="auto";
+        }else{
+        	this.images[this.i-1].style.left="0";
+        	this.images[this.i-2].style.right="0";
+        	this.images[this.i-2].style.left="auto";
+        }
+    }
+}
